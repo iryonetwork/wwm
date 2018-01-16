d7257fb69d6ea8325dd88200a32505393533ab93
Matevz Mihalic <matevz.mihalic@3fs.si>
2018-01-16 13:21:35 +0100

    Move cloud auth to tls serving

--- cmd/cloudAuth/main.go
+++ cmd/cloudAuth/main.go
@@ -20,7 +20,9 @@ func main() {
 	}
 
 	// initialize storage
-	storage, err := auth.New("/tmp/auth.db")
+	// TODO: get key from vault
+	key := []byte{0xe9, 0xf8, 0x2d, 0xf9, 0xc4, 0x14, 0xc1, 0x41, 0xdb, 0x87, 0x31, 0x1a, 0x95, 0x79, 0x5, 0xbf, 0x71, 0x12, 0x30, 0xd3, 0x2d, 0x8b, 0x59, 0x9d, 0x27, 0x13, 0xfa, 0x84, 0x55, 0x63, 0x64, 0x64}
+	storage, err := auth.New("/tmp/auth.db", key)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -30,8 +32,10 @@ func main() {
 
 	api := operations.NewCloudAuthAPI(swaggerSpec)
 	server := restapi.NewServer(api)
-	server.Port = 80
-	server.EnabledListeners = []string{"http"}
+	server.TLSPort = 443
+	server.TLSCertificate = "/certs/cloudAuth.pem"
+	server.TLSCertificateKey = "/certs/cloudAuth-key.pem"
+	server.EnabledListeners = []string{"https"}
 	defer server.Shutdown()
 
 	authHandlers := authenticator.NewHandlers(auth)
