83302ced4877aced86648d7f58df0478dccc19d3
Matevz Mihalic <matevz.mihalic@3fs.si>
2018-02-12 17:03:46 +0100

    Close alert on route change

--- frontend/cloud/src/containers/app/index.js
+++ frontend/cloud/src/containers/app/index.js
@@ -1,64 +1,88 @@
 import React from "react"
 import { Route, Link, NavLink } from "react-router-dom"
+import { connect } from "react-redux"
+import { bindActionCreators } from "redux"
+
 import Home from "../home"
 import Rules from "../rules"
 import Alert from "../alert"
 import Users from "../users"
 import UserDetail from "../users/detail"
 import Roles from "../roles"
+import { close } from "../../modules/alert"
 
-const App = () => (
-    <div>
-        <header>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div className="container">
-                    <Link className="navbar-brand" to="/">
-                        Home
-                    </Link>
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-toggle="collapse"
-                        data-target="#navbarText"
-                        aria-controls="navbarText"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon" />
-                    </button>
+class App extends React.Component {
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.location.pathname !== this.props.location.pathname) {
+            this.props.close()
+        }
+    }
 
-                    <div className="collapse navbar-collapse">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/users">
-                                    Users
-                                </NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/roles">
-                                    Roles
-                                </NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/rules">
-                                    ACL
-                                </NavLink>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
+    render() {
+        return (
+            <div>
+                <header>
+                    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                        <div className="container">
+                            <Link className="navbar-brand" to="/">
+                                Home
+                            </Link>
+                            <button
+                                className="navbar-toggler"
+                                type="button"
+                                data-toggle="collapse"
+                                data-target="#navbarText"
+                                aria-controls="navbarText"
+                                aria-expanded="false"
+                                aria-label="Toggle navigation"
+                            >
+                                <span className="navbar-toggler-icon" />
+                            </button>
 
-        <main className="container">
-            <Alert />
-            <Route exact path="/" component={Home} />
-            <Route exact path="/users" component={Users} />
-            <Route path="/users/:id" component={UserDetail} />
-            <Route path="/roles" component={Roles} />
-            <Route exact path="/rules" component={Rules} />
-        </main>
-    </div>
-)
+                            <div className="collapse navbar-collapse">
+                                <ul className="navbar-nav mr-auto">
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" to="/users">
+                                            Users
+                                        </NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" to="/roles">
+                                            Roles
+                                        </NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" to="/rules">
+                                            ACL
+                                        </NavLink>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </header>
 
-export default App
+                <main className="container">
+                    <Alert />
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/users" component={Users} />
+                    <Route path="/users/:id" component={UserDetail} />
+                    <Route path="/roles" component={Roles} />
+                    <Route exact path="/rules" component={Rules} />
+                </main>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({})
+
+const mapDispatchToProps = dispatch =>
+    bindActionCreators(
+        {
+            close
+        },
+        dispatch
+    )
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
