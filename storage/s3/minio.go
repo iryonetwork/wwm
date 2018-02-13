f03d55b7d24c3f225f3d783668f75ec4f8e9a1d7
Mateusz Krasucki <mateusz.krasucki@3fs.si>
2018-02-13 15:59:51 +0100

    ADD BucketExists wrapper for minio client
    - Solves bug in which BucketExists method of minio pkg returns
      error instead of fasle when response is 404 on bucket HEAD call.

--- storage/s3/minio.go
+++ storage/s3/minio.go
@@ -13,3 +13,12 @@ type iminio struct {
 func (m iminio) GetObject(bucketName, objectName string, opts minio.GetObjectOptions) (object.Object, error) {
 	return m.Client.GetObject(bucketName, objectName, opts)
 }
+
+// BucketExists is a wrapper for original BucketExists method to fix incorrect handling of 404 returned as response
+func (m iminio) BucketExists(bucketName string) (bool, error) {
+	exists, err := m.Client.BucketExists(bucketName)
+	if err != nil && minio.ToErrorResponse(err).Code == "NoSuchBucket" {
+		return false, nil
+	}
+	return exists, err
+}
