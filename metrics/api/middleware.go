2fb05d44bf6ec204e107f99f3b29e71695e6475d
Mateusz Krasucki <mateusz.krasucki@3fs.si>
2018-02-16 15:27:20 +0100

    ADD API metrics middleware
    - Adds generalised middleware for API metrics
    - Adds prometheus metrics serving method
    - Related: #8

---
+++ metrics/api/middleware.go
@@ -0,0 +1,77 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+
+	"github.com/iryonetwork/wwm/utils"
+)
+
+type Metrics interface {
+	Middleware(next http.Handler) http.Handler
+	WithURLSanitize(sanitize utils.URLSanitize) Metrics
+}
+
+type metrics struct {
+	requestSeconds *prometheus.HistogramVec
+	urlSanitize    utils.URLSanitize
+}
+
+type codeRecordingResponseWriter struct {
+	http.ResponseWriter
+	code int
+}
+
+func (m *metrics) Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		method := r.Method
+		code := 500 // status code to use when completely fails
+
+		path := r.URL.Path
+		if m.urlSanitize != nil {
+			path = m.urlSanitize(path)
+		}
+
+		// Make sure we record even if fails
+		defer func() {
+			duration := time.Since(start)
+			m.requestSeconds.
+				With(prometheus.Labels{"path": path, "method": method, "code": fmt.Sprintf("%d", code)}).
+				Observe(duration.Seconds())
+		}()
+
+		cw := &codeRecordingResponseWriter{w, 200}
+
+		next.ServeHTTP(cw, r)
+
+		code = cw.code
+	})
+}
+
+func NewMetrics(namespace string, subsystem string) Metrics {
+	h := prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: namespace,
+		Subsystem: subsystem,
+		Name:      "request_seconds",
+		Help:      "Time taken to serve API request",
+	}, []string{"path", "method", "code"})
+
+	// Register metrics
+	prometheus.MustRegister(h)
+
+	return &metrics{requestSeconds: h}
+}
+
+func (m *metrics) WithURLSanitize(sanitize utils.URLSanitize) Metrics {
+	m.urlSanitize = sanitize
+	return m
+}
+
+func (w *codeRecordingResponseWriter) WriteHeader(code int) {
+	w.code = code
+	w.ResponseWriter.WriteHeader(code)
+}
---
+++ metrics/server.go
@@ -0,0 +1,37 @@
+package metrics
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/iryonetwork/wwm/metrics/api"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+func ServePrometheusMetrics(ctx context.Context, addr string, namespace string) error {
+	// initialize metrics middleware
+	m := api.NewMetrics("metrics", "")
+
+	path := "/metrics"
+	if namespace != "" {
+		path = fmt.Sprintf("/%s/metrics", namespace)
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle(path, prometheus.Handler())
+	s := &http.Server{
+		Addr:    addr,
+		Handler: m.Middleware(mux),
+	}
+
+	go func() {
+		<-ctx.Done()
+		s.Shutdown(ctx)
+	}()
+
+	log.Printf("Starting metrics server at %s%s", addr, path)
+
+	return s.ListenAndServe()
+}
