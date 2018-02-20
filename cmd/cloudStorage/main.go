e216b71652b17aa289c8ffc914b51f5d5bd82343
Mateusz Krasucki <mateusz.krasucki@3fs.si>
2018-02-20 12:55:37 +0100

    UPD storage API swagger schema
    - Add tags for local/cloud.
    - Add new endpoint (syncBucketList) for fetching list of buckets
      and BucketDescriptor object definition.
    - Add new endpoint (syncFileList) for listing all files in bucket
      including files that are marked as removed.
    - Aforementioned changes forced changes in *Storage and storageSync
      as names of packages generated by go-swagger changed.
    - Related: #7

--- cmd/cloudStorage/main.go
+++ cmd/cloudStorage/main.go
@@ -74,16 +74,16 @@ func main() {
 
 	api.TokenAuth = storageHandlers.GetUserIDFromToken
 	api.APIAuthorizer = storageHandlers.Authorizer()
-	api.StorageFileListHandler = storageHandlers.FileList()
-	api.StorageFileGetHandler = storageHandlers.FileGet()
-	api.StorageFileGetVersionHandler = storageHandlers.FileGetVersion()
-	api.StorageFileListVersionsHandler = storageHandlers.FileListVersions()
-	api.StorageFileNewHandler = storageHandlers.FileNew()
-	api.StorageFileUpdateHandler = storageHandlers.FileUpdate()
-	api.StorageFileDeleteHandler = storageHandlers.FileDelete()
-	api.StorageSyncFileMetadataHandler = storageHandlers.SyncFileMetadata()
-	api.StorageSyncFileHandler = storageHandlers.SyncFile()
-	api.StorageSyncFileDeleteHandler = storageHandlers.SyncFileDelete()
+	api.FileListHandler = storageHandlers.FileList()
+	api.FileGetHandler = storageHandlers.FileGet()
+	api.FileGetVersionHandler = storageHandlers.FileGetVersion()
+	api.FileListVersionsHandler = storageHandlers.FileListVersions()
+	api.FileNewHandler = storageHandlers.FileNew()
+	api.FileUpdateHandler = storageHandlers.FileUpdate()
+	api.FileDeleteHandler = storageHandlers.FileDelete()
+	api.SyncFileMetadataHandler = storageHandlers.SyncFileMetadata()
+	api.SyncFileHandler = storageHandlers.SyncFile()
+	api.SyncFileDeleteHandler = storageHandlers.SyncFileDelete()
 
 	api.RegisterConsumer("*/*", &WildcardConsumer{})
 
