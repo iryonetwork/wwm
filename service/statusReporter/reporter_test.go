1fc9fca5e39d089006ee8eba0fac3b58962e130b
Mateusz Krasucki <mateusz.krasucki@3fs.si>
2018-03-26 10:32:47 +0200

    UPD status reporter test
    - Increase polling interval for better control of test calls.

--- service/statusReporter/reporter_test.go
+++ service/statusReporter/reporter_test.go
@@ -18,7 +18,7 @@ import (
 
 var (
 	// interval for status calls in tests
-	interval = time.Duration(5 * time.Millisecond)
+	interval = time.Duration(50 * time.Millisecond)
 )
 
 func TestStatus(t *testing.T) {
