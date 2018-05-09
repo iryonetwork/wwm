d6eb43b581c65067a8ceb5e3f30aee00f33b897d
Mateusz Krasucki <mateusz.krasucki@3fs.si>
2018-05-09 02:09:27 +0200

    ADD cloudStatusReporter cmd
    - Add cmd for reporting status at cloud level.

---
+++ cmd/cloudStatusReporter/main.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/rs/cors"
+	"github.com/rs/zerolog"
+
+	"github.com/iryonetwork/wwm/log"
+	"github.com/iryonetwork/wwm/metrics/api"
+	metricsServer "github.com/iryonetwork/wwm/metrics/server"
+	"github.com/iryonetwork/wwm/service/statusReporter"
+	"github.com/iryonetwork/wwm/service/statusReporter/polling"
+)
+
+func main() {
+	// initialize logger
+	logger := zerolog.New(os.Stdout).With().
+		Timestamp().
+		Str("service", "cloudStatusReporter").
+		Logger()
+
+	// create context with cancel func
+	ctx, cancelContext := context.WithCancel(context.Background())
+	defer cancelContext()
+
+	// get config
+	cfg, err := GetConfig()
+	if err != nil {
+		logger.Fatal().Err(err).Msg("failed to get config")
+	}
+	logger.Print(cfg)
+
+	// initialize status reporter
+	r := statusReporter.New(logger)
+
+	// prepare default config for URL status polling components
+	defaultPollingCfg := polling.Cfg{
+		Interval:       &cfg.Interval,
+		CountThreshold: &cfg.CountThreshold,
+		StatusValidity: &cfg.StatusValidity,
+	}
+
+	// add URL status polling components
+	for id, c := range cfg.Components.Cloud {
+		pollingCfg, timeout := c.getConfig(cfg.DefaultTimeout, &defaultPollingCfg)
+
+		var url polling.URLStatusEndpoint
+		switch c.URLType {
+		case polling.TypeInternalURL:
+			url = polling.NewInternalURL(c.URL, timeout)
+		default:
+			url = polling.NewExternalURL(c.URL, timeout)
+		}
+		p := polling.New(url, &pollingCfg, logger)
+		p.Start(ctx)
+		r.AddComponent(statusReporter.Cloud, id, p)
+	}
+	for id, c := range cfg.Components.External {
+		pollingCfg, timeout := c.getConfig(cfg.DefaultTimeout, &defaultPollingCfg)
+
+		var url polling.URLStatusEndpoint
+		switch c.URLType {
+		case polling.TypeInternalURL:
+			url = polling.NewInternalURL(c.URL, timeout)
+		default:
+			url = polling.NewExternalURL(c.URL, timeout)
+		}
+		p := polling.New(url, &pollingCfg, logger)
+		p.Start(ctx)
+		r.AddComponent(statusReporter.External, id, p)
+	}
+
+	// initialize metrics middleware
+	m := api.NewMetrics("cloudStatusReporter", "")
+
+	// setup handler
+	handler := cors.New(cors.Options{
+		AllowedMethods: []string{"GET"},
+		AllowedHeaders: []string{"Authorization", "Content-Type"},
+	}).Handler(m.Middleware(log.APILogMiddleware(r.Handler("status"), logger)))
+
+	server := &http.Server{
+		Addr:    fmt.Sprintf("%s:%d", cfg.ServerHost, cfg.ServerPort),
+		Handler: handler,
+	}
+
+	// Start servers
+	// create exit channel that is used to wait for all servers goroutines to exit orederly and carry the errors
+	exitCh := make(chan error, 2)
+
+	// start serving metrics
+	go func() {
+		exitCh <- metricsServer.ServePrometheusMetrics(ctx, fmt.Sprintf("%s:%d", cfg.ServerHost, cfg.MetricsPort), cfg.MetricsNamespace, logger)
+	}()
+	go func() {
+		defer server.Close()
+
+		errCh := make(chan error)
+		go func() {
+			logger.Info().Msgf("Starting status reporter server at %s", server.Addr)
+			errCh <- server.ListenAndServeTLS(cfg.CertPath, cfg.KeyPath)
+		}()
+
+		for {
+			select {
+			case err := <-errCh:
+				exitCh <- err
+				return
+			case <-ctx.Done():
+				exitCh <- fmt.Errorf("StatusReporter server exiting because of cancelled context")
+				//do nothing except deferred cleanup
+				return
+			}
+		}
+	}()
+
+	// run cleanup when sigint or sigterm is received or error on starting server happened
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		defer cancelContext()
+
+		for {
+			select {
+			case err := <-exitCh:
+				logger.Info().Msg("exiting application because of exiting server goroutine")
+				// pass error back to channel satisfy exit condition
+				exitCh <- err
+				return
+			case <-signalChan:
+				logger.Info().Msg("received interrupt")
+				return
+			}
+		}
+	}()
+
+	<-ctx.Done()
+	for i := 0; i < 2; i++ {
+		err := <-exitCh
+		if err != nil {
+			logger.Debug().Err(err).Msg("gouroutine exit message")
+		}
+	}
+}
