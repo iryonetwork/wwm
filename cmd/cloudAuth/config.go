db601a4ec2446cdefb4ae870ba1b8c2db95192d8
Mateusz Krasucki <mateusz.krasucki@3fs.si>
2018-05-15 12:17:24 +0300

    UPD allow multiple auth storage init files

--- cmd/cloudAuth/config.go
+++ cmd/cloudAuth/config.go
@@ -3,11 +3,13 @@ package main
 import (
 	"io/ioutil"
 	"reflect"
+	"strings"
 
 	"github.com/caarlos0/env"
 	"gopkg.in/yaml.v2"
 
 	"github.com/iryonetwork/wwm/config"
+	"github.com/iryonetwork/wwm/gen/auth/models"
 	"github.com/iryonetwork/wwm/storage/auth"
 )
 
@@ -23,7 +25,7 @@ type Config struct {
 	ServiceCertsAndPaths Services `env:"SERVICES_FILEPATH" envDefault:"/serviceCertsAndPaths.yml"`
 
 	// filepath to yaml
-	StorageInitData auth.InitData `env:"STORAGE_INIT_DATA_FILEPATH" envDefault:"/storageInitData.yml"`
+	StorageInitData auth.InitData `env:"STORAGE_INIT_DATA_FILEPATHS" envDefault:"/rolesAndRules.yml"`
 }
 
 // Services is a wrapper struct for map of allowed services certs and paths
@@ -67,18 +69,40 @@ func parseServiceCertsAndPaths(filepath string) (interface{}, error) {
 	return serviceCertsAndPaths, nil
 }
 
-func parseStorageInitData(filepath string) (interface{}, error) {
-	storageInitData := auth.InitData{}
+func parseStorageInitData(filepaths string) (interface{}, error) {
+	filepathsSlice := strings.Split(filepaths, ",")
 
-	yamlFile, err := ioutil.ReadFile(filepath)
-
-	if err != nil {
-		return storageInitData, nil
+	storageInitData := auth.InitData{
+		Locations:     []*models.Location{},
+		Organizations: []*models.Organization{},
+		Clinics:       []*models.Clinic{},
+		Roles:         []*models.Role{},
+		Rules:         []*models.Rule{},
+		Users:         []*models.User{},
+		UserRoles:     []*models.UserRole{},
 	}
 
-	err = yaml.Unmarshal(yamlFile, &storageInitData)
-	if err != nil {
-		return nil, err
+	for _, filepath := range filepathsSlice {
+		initData := auth.InitData{}
+		yamlFile, err := ioutil.ReadFile(filepath)
+
+		if err != nil {
+			return storageInitData, nil
+		}
+
+		err = yaml.Unmarshal(yamlFile, &initData)
+		if err != nil {
+			return nil, err
+		}
+
+		storageInitData.Locations = append(storageInitData.Locations, initData.Locations...)
+		storageInitData.Organizations = append(storageInitData.Organizations, initData.Organizations...)
+		storageInitData.Clinics = append(storageInitData.Clinics, initData.Clinics...)
+		storageInitData.Roles = append(storageInitData.Roles, initData.Roles...)
+		storageInitData.Rules = append(storageInitData.Rules, initData.Rules...)
+		storageInitData.Users = append(storageInitData.Users, initData.Users...)
+		storageInitData.UserRoles = append(storageInitData.UserRoles, initData.UserRoles...)
+
 	}
 
 	return storageInitData, nil
