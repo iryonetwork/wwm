d34a73a45fd20639b41059ddf46d6e4ef12a0712
j-justin <janez.justin0@gmail.com>
2018-09-05 11:26:55 +0200

    UPD version style

--- frontend/shared/containers/version/index.js
+++ frontend/shared/containers/version/index.js
@@ -3,4 +3,4 @@ import "./style.css"
 
 const defaultVersion = "dev"
 
-export default () => (<div id="version" class="version" >{process.env.REACT_APP_VERSION || defaultVersion}</div>)
+export default () => (<div class="appversion" >{process.env.REACT_APP_VERSION || defaultVersion}</div>)
