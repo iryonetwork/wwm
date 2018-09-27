754843a6332b37cca03fea3fdf7092eeaf51b23d
Mateusz Krasucki <mateusz.krasucki@3fs.si>
2018-09-27 17:53:42 +0200

    UPD make cert scripts more universal
    - Updates certs generation scripts to work across systems.
      Problems were identified when running from windows.

--- bin/tls/createProfileCert.sh
+++ bin/tls/createProfileCert.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
-cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json \
-    -profile=$1 $2.json | cfssljson -bare $2
+cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=$1 $2.json | cfssljson -bare $2
